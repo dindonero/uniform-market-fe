1498cb57bc3318099a659f1cca0a8e434eb670d2
0xChefMike <dinis.palha@gmail.com>
2025-07-29 13:23:53 +0100

    force vercel redeploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <Head>
           <title>COMMUNITAS Energy Market</title>
           <link rel="icon" href="/communitas.ico" />
-          <meta name="google-site-verification" content="6Gtzx3veON661xdrUMbM-KbCCO2MrRfVAWDu-tlZy84" />
+          <meta name="google-site-verification" content="6Gtzx3veON661xdrUMbM-KbCCO2MrRfVAWDu-tlZy84"  />
         </Head>
         <header>
           <div
